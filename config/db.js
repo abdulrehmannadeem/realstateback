e372c2eca34400b6080372059bba3bef0c8b2f43
Abdul Rehman <abdulrehmannadeem@gmail.com>
2026-05-13 14:49:18 +0500

    Fix db.js - restore mysql require

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
+const mysql = require('mysql2');
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT, // <-- We added this
+    port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: false // <-- Aiven strictly requires this
+        rejectUnauthorized: false
     },
     waitForConnections: true,
     connectionLimit: 10,
