86d3fa8fc105f27e413c0b85c0ca1a3be93ececc
Abdul Rehman <abdulrehmannadeem@gmail.com>
2026-05-13 14:45:35 +0500

    Only load .env in development not production

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
